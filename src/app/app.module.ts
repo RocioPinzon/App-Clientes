678278aeac9763f9e395b20009331e0f7644ed63
RocioPinzon <pinzonbayonr@gmail.com>
2023-01-14 14:18:30 +0100

    Aplicacion Clientes | Creacion componente clientes.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { DirectivaComponent } from './directiva/directiva.component';
+import { ClientesComponent } from './clientes/clientes.component';
+import { ClienteComponent } from './clientes/cliente/cliente.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    DirectivaComponent
+    DirectivaComponent,
+    ClientesComponent,
+    ClienteComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/clientes/clientes.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-clientes',
+  templateUrl: './clientes.component.html',
+})
+export class ClientesComponent {
+
+  constructor(){}
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    
+  }
+}
