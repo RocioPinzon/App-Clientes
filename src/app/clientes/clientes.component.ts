2c169374451b8dc11f663f52e2e61a67dc9f67e4
RocioPinzon <pinzonbayonr@gmail.com>
2023-01-14 14:51:39 +0100

    Aplicacion Clientes | Creacion clientes.json.ts con lista de objetos y clase Servicio Cliente Service mas Inyeccion dependencia

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,16 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Cliente } from './cliente';
+import { ClienteService } from './cliente.service';
 
 @Component({
   selector: 'app-clientes',
   templateUrl: './clientes.component.html',
 })
-export class ClientesComponent {
 
-  constructor(){}
+export class ClientesComponent implements OnInit {
+
+  clientes: Cliente[];
+
+  constructor(private clienteService: ClienteService){}
 
   ngOnInit(): void {
-    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    //Add 'implements OnInit' to the class.
-    
+    this.clientes = this.clienteService.getClientes();
   }
 }
---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,10 @@
+import { Cliente } from './cliente';
+
+export const CLIENTES: Cliente[] =  [
+
+    {id: 1, nombre: 'Rocio', apellido: 'Pinzón', email: 'pinzonbayonr@gmail.com', createAt: '2023-01-14'},
+    {id: 2, nombre: 'Alba', apellido: 'Pinzón', email: 'alba@alba.com', createAt: '2023-01-14'},
+    {id: 3, nombre: 'Ruth', apellido: 'Azcuaga', email: 'ruth@ruth.com', createAt: '2023-01-14'},
+    {id: 4, nombre: 'Mara', apellido: 'Bayón', email: 'mara@mara.com', createAt: '2023-01-14'},
+    {id: 5, nombre: 'Karlos', apellido: 'Azcuaga', email: 'karlos@karlos.com', createAt: '2023-01-14'}
+  ];
