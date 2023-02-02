6c8c8df7162c780b1958f085a76842f0466111f4
RocioPinzon <pinzonbayonr@gmail.com>
2023-02-02 13:05:23 +0100

    Aplicacion Clientes | Agregamos HttpModule

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -2,15 +2,21 @@ import { Injectable } from '@angular/core';
 import { CLIENTES } from './clientes.json';
 import { Cliente } from './cliente';
 import { of, Observable } from 'rxjs';
-
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
 
-  constructor() { }
+  private urlEndPoint:string = 'http://localhost:8080/api/clientes'
+  constructor(private http:HttpClient) { }
 
   getClientes(): Observable<Cliente[]>{
-    return of(CLIENTES); // of() Convertimos nuestro listado clientes en un observable, en este caso en un string.
+    //return of(CLIENTES); // of() Convertimos nuestro listado clientes en un observable, en este caso en un string.
+    return this.http.get<Cliente[]>(this.urlEndPoint)  //una forma casteada
+    /*return this.http.get(this.urlEndPoint).pipe( //metodho pipe, permite agregar mas operadores.
+      map(response => response as Cliente[])
+    );*/
   };
 }
