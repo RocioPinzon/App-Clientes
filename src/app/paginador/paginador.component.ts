77d42659466d55abcfd1b55f96cd1d1a97661cac
RocioPinzon <pinzonbayonr@gmail.com>
2023-02-13 14:16:05 +0100

    Aplicacion clientes | Inyectando paginador. Uso decorador @Input

--- src/app/paginador/paginador.component.ts
+++ src/app/paginador/paginador.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'paginador-nav',
   templateUrl: './paginador.component.html'
 })
 export class PaginadorComponent {
+  
+  @Input() paginador:any;
 
+  paginas: number[];
+
+  constructor(){
+
+  }
+
+  ngOnInit(): void {
+    this.paginas = new Array(this.paginador.totalPages).fill(0).map((_valor, indice) => indice+1);
+  }
 }
